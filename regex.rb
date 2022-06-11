fd11cb5775db796ebe2493bc4dc02c3c4e4f8a04
kockono <kockonoop@gmail.com>
2022-06-10 19:22:42 -0600

    expresiones regulares

---
+++ regex.rb
@@ -0,0 +1,6 @@
+# Regex para gmail
+is_gmail_regex = /\+@gmail.com
+
+simon@gmail.com.match is_gmail_regex
+# nil Significa que el valor es negativo no hizo match
+number_regex = /\d+/ # Este string hacer match con numeros
